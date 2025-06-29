32c4f67767f780fcbddc0888d8a22a33a0cda22e
NewChoBo <whwjdrb1009@gmail.com>
2025-06-29 21:14:20 +0900

    fix: 개발설정 적용

---
+++ lib/widgets/frame_layout.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FrameLayout extends StatelessWidget {
+  const FrameLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(padding: EdgeInsets.all(8.0), child: Column());
+  }
+}
