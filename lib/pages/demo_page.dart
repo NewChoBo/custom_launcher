8adba1d132b2ef96d10e4cb683b50357e85dd808
NewChoBo <whwjdrb1009@gmail.com>
2025-07-06 13:02:46 +0900

    design: demo 화면 개선

--- lib/pages/demo_page.dart
+++ lib/pages/demo_page.dart
@@ -7,23 +7,43 @@ class DemoPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       backgroundColor: Colors.transparent,
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              spacing: 10,
-              children: <Widget>[
-                Expanded(child: Card()),
-                Expanded(child: Card()),
-                Expanded(child: Card()),
-              ],
+      body: Expanded(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                spacing: 2,
+                children: <Widget>[
+                  Expanded(
+                    child: Row(
+                      spacing: 2,
+                      children: <Widget>[Card(), Card(), Card()],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Row(
+                      spacing: 2,
+                      children: <Widget>[
+                        Card(),
+                        Card(),
+                        Card(),
+                        Card(),
+                        Card(),
+                        Card(),
+                        Card(),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -34,58 +54,60 @@ class Card extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      clipBehavior: Clip.hardEdge,
-      decoration: BoxDecoration(
-        color: const Color(0xFF1F2123),
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(30),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                const Text(
-                  'Euro',
-                  style: TextStyle(
+    return Expanded(
+      child: Container(
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+          color: const Color(0xFF1F2123).withValues(alpha: 0.5),
+          borderRadius: BorderRadius.zero,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const Text(
+                    'Euro',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: <Widget>[
+                      const Text(
+                        '6 428 ',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      const SizedBox(width: 5),
+                      Text(
+                        'EUR',
+                        style: TextStyle(
+                          color: Colors.white.withValues(alpha: 0.8),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Transform.scale(
+                scale: 2.2,
+                child: Transform.translate(
+                  offset: const Offset(-5, 12),
+                  child: const Icon(
+                    Icons.euro_rounded,
                     color: Colors.white,
-                    fontSize: 32,
-                    fontWeight: FontWeight.w600,
+                    size: 88,
                   ),
                 ),
-                const SizedBox(height: 10),
-                Row(
-                  children: <Widget>[
-                    const Text(
-                      '6 428 ',
-                      style: TextStyle(color: Colors.white, fontSize: 20),
-                    ),
-                    const SizedBox(width: 5),
-                    Text(
-                      'EUR',
-                      style: TextStyle(
-                        color: Colors.white.withValues(alpha: 0.8),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Transform.scale(
-              scale: 2.2,
-              child: Transform.translate(
-                offset: const Offset(-5, 12),
-                child: const Icon(
-                  Icons.euro_rounded,
-                  color: Colors.white,
-                  size: 88,
-                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
